d570f244cfa222150d8ae6704b74bce545cc0571
Lucas dos Santos Soares <lucas.soares@bplus.com.br.local>
2021-01-20 13:41:09 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD</h1>
+      <div className="form">
+        <label>Movie Name:</label>
+        <input type="text" name="movieName"/>
+        <label>Review</label>
+        <input type="text" name="review"/>
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
